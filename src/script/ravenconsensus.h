ea56f5a72d5de083e095e32b502155c7961f9326
gawu <gawu@Ctrip.com>
2018-04-13 18:17:32 +0800

    raven consensus

--- src/script/ravenconsensus.h
+++ src/script/ravenconsensus.h
@@ -36,44 +36,44 @@ extern "C" {
 
 #define CARROTCONSENSUS_API_VER 1
 
-typedef enum ravenconsensus_error_t
+typedef enum carrotconsensus_error_t
 {
-    ravenconsensus_ERR_OK = 0,
-    ravenconsensus_ERR_TX_INDEX,
-    ravenconsensus_ERR_TX_SIZE_MISMATCH,
-    ravenconsensus_ERR_TX_DESERIALIZE,
-    ravenconsensus_ERR_AMOUNT_REQUIRED,
-    ravenconsensus_ERR_INVALID_FLAGS,
-} ravenconsensus_error;
+    carrotconsensus_ERR_OK = 0,
+    carrotconsensus_ERR_TX_INDEX,
+    carrotconsensus_ERR_TX_SIZE_MISMATCH,
+    carrotconsensus_ERR_TX_DESERIALIZE,
+    carrotconsensus_ERR_AMOUNT_REQUIRED,
+    carrotconsensus_ERR_INVALID_FLAGS,
+} carrotconsensus_error;
 
 /** Script verification flags */
 enum
 {
-    ravenconsensus_SCRIPT_FLAGS_VERIFY_NONE                = 0,
-    ravenconsensus_SCRIPT_FLAGS_VERIFY_P2SH                = (1U << 0), // evaluate P2SH (BIP16) subscripts
-    ravenconsensus_SCRIPT_FLAGS_VERIFY_DERSIG              = (1U << 2), // enforce strict DER (BIP66) compliance
-    ravenconsensus_SCRIPT_FLAGS_VERIFY_NULLDUMMY           = (1U << 4), // enforce NULLDUMMY (BIP147)
-    ravenconsensus_SCRIPT_FLAGS_VERIFY_CHECKLOCKTIMEVERIFY = (1U << 9), // enable CHECKLOCKTIMEVERIFY (BIP65)
-    ravenconsensus_SCRIPT_FLAGS_VERIFY_CHECKSEQUENCEVERIFY = (1U << 10), // enable CHECKSEQUENCEVERIFY (BIP112)
-    ravenconsensus_SCRIPT_FLAGS_VERIFY_WITNESS             = (1U << 11), // enable WITNESS (BIP141)
-    ravenconsensus_SCRIPT_FLAGS_VERIFY_ALL                 = ravenconsensus_SCRIPT_FLAGS_VERIFY_P2SH | ravenconsensus_SCRIPT_FLAGS_VERIFY_DERSIG |
-                                                               ravenconsensus_SCRIPT_FLAGS_VERIFY_NULLDUMMY | ravenconsensus_SCRIPT_FLAGS_VERIFY_CHECKLOCKTIMEVERIFY |
-                                                               ravenconsensus_SCRIPT_FLAGS_VERIFY_CHECKSEQUENCEVERIFY | ravenconsensus_SCRIPT_FLAGS_VERIFY_WITNESS
+    carrotconsensus_SCRIPT_FLAGS_VERIFY_NONE                = 0,
+    carrotconsensus_SCRIPT_FLAGS_VERIFY_P2SH                = (1U << 0), // evaluate P2SH (BIP16) subscripts
+    carrotconsensus_SCRIPT_FLAGS_VERIFY_DERSIG              = (1U << 2), // enforce strict DER (BIP66) compliance
+    carrotconsensus_SCRIPT_FLAGS_VERIFY_NULLDUMMY           = (1U << 4), // enforce NULLDUMMY (BIP147)
+    carrotconsensus_SCRIPT_FLAGS_VERIFY_CHECKLOCKTIMEVERIFY = (1U << 9), // enable CHECKLOCKTIMEVERIFY (BIP65)
+    carrotconsensus_SCRIPT_FLAGS_VERIFY_CHECKSEQUENCEVERIFY = (1U << 10), // enable CHECKSEQUENCEVERIFY (BIP112)
+    carrotconsensus_SCRIPT_FLAGS_VERIFY_WITNESS             = (1U << 11), // enable WITNESS (BIP141)
+    carrotconsensus_SCRIPT_FLAGS_VERIFY_ALL                 = carrotconsensus_SCRIPT_FLAGS_VERIFY_P2SH | carrotconsensus_SCRIPT_FLAGS_VERIFY_DERSIG |
+                                                               carrotconsensus_SCRIPT_FLAGS_VERIFY_NULLDUMMY | carrotconsensus_SCRIPT_FLAGS_VERIFY_CHECKLOCKTIMEVERIFY |
+                                                               carrotconsensus_SCRIPT_FLAGS_VERIFY_CHECKSEQUENCEVERIFY | carrotconsensus_SCRIPT_FLAGS_VERIFY_WITNESS
 };
 
 /// Returns 1 if the input nIn of the serialized transaction pointed to by
 /// txTo correctly spends the scriptPubKey pointed to by scriptPubKey under
 /// the additional constraints specified by flags.
 /// If not nullptr, err will contain an error/success code for the operation
-EXPORT_SYMBOL int ravenconsensus_verify_script(const unsigned char *scriptPubKey, unsigned int scriptPubKeyLen,
+EXPORT_SYMBOL int carrotconsensus_verify_script(const unsigned char *scriptPubKey, unsigned int scriptPubKeyLen,
                                                  const unsigned char *txTo        , unsigned int txToLen,
-                                                 unsigned int nIn, unsigned int flags, ravenconsensus_error* err);
+                                                 unsigned int nIn, unsigned int flags, carrotconsensus_error* err);
 
-EXPORT_SYMBOL int ravenconsensus_verify_script_with_amount(const unsigned char *scriptPubKey, unsigned int scriptPubKeyLen, int64_t amount,
+EXPORT_SYMBOL int carrotconsensus_verify_script_with_amount(const unsigned char *scriptPubKey, unsigned int scriptPubKeyLen, int64_t amount,
                                     const unsigned char *txTo        , unsigned int txToLen,
-                                    unsigned int nIn, unsigned int flags, ravenconsensus_error* err);
+                                    unsigned int nIn, unsigned int flags, carrotconsensus_error* err);
 
-EXPORT_SYMBOL unsigned int ravenconsensus_version();
+EXPORT_SYMBOL unsigned int carrotconsensus_version();
 
 #ifdef __cplusplus
 } // extern "C"
